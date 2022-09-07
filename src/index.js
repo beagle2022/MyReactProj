72229eea83cde3ab8ba20c292210e7f669bb6ddb
Abhishek Bhattachaya <ab7607@rediffmail.com>
2022-09-07 17:06:37 +0530

    modified index.js with reactdom/client

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counters from './components/counters';
 
-ReactDOM.render(<Counters />, document.getElementById("root"));
+//ReactDOM.render(<Counters />, document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+
+  <React.StrictMode>
+
+    <Counters />
+
+  </React.StrictMode>
+
+);
